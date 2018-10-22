2fc9a55e4d26baf2bdc7118db203f836f2626365
Carolina Kimbrough <carolinamargaritak@gmail.com>
2018-10-22 14:16:38 -0500

    changed main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,42 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
+	
+	public static Stage stage;
+	
+	/** 
+	 * Loads the FXML file, sets the scene onto stage
+	*/
 	@Override
 	public void start(Stage primaryStage) {
+		
+		stage = primaryStage; // Initializing the stage
+		
 		try {
 			
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			// Load the FXML document
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation( getClass().getResource("/Main.fxml") );
+			
+			// Load the layout from the FXML and add it to the scene
+			AnchorPane layout = (AnchorPane) loader.load();
+			Scene scene = new Scene( layout );
+			
+			// Style sheet
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			
+			// Set the scene to stage and show the stage to user
+			stage.setScene( scene );
+			stage.setTitle( "Sudoku" );
+			stage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
