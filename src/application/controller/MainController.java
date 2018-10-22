9375aa9e8ec0a18984f6ac3b14bdcc2384ef5503
Carolina Kimbrough <carolinamargaritak@gmail.com>
2018-10-22 14:43:25 -0500

    added fxml stuff

---
+++ src/application/controller/MainController.java
@@ -0,0 +1,19 @@
+package application.controller;
+
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+public class MainController implements EventHandler<ActionEvent> {
+	
+	@Override
+	public void handle(ActionEvent event) {
+		
+		System.out.println("Hello");
+
+	}
+	
+
+}
