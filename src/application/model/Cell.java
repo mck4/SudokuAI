9216936d877abd63052e82bc36e402da3509a18f
zabrigo <zoeyabrigo@gmail.com>
2018-10-23 12:52:42 -0500

    created the board and cell classes, nothing in them really for now

---
+++ src/application/model/Cell.java
@@ -0,0 +1,30 @@
+package application.model;
+
+public class Cell {
+	
+	
+	int value;
+	boolean isEmpty;
+	
+	public Cell(int value, boolean isEmpty) {
+		this.value = value;
+		this.isEmpty = isEmpty;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public boolean isEmpty() {
+		return isEmpty;
+	}
+
+	public void setEmpty(boolean isEmpty) {
+		this.isEmpty = isEmpty;
+	}
+
+}
