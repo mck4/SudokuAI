fda3d4b82734f3cff36ff8cffb47a90cc8ffae1a
Carolina Kimbrough <carolinamargaritak@gmail.com>
2018-10-23 13:07:25 -0500

    added comments to cell.java

--- src/application/model/Cell.java
+++ src/application/model/Cell.java
@@ -2,26 +2,32 @@ package application.model;
 
 public class Cell {	
 	
+	// Variables
 	int value;
 	boolean isEmpty;
 	
+	// Constructor
 	public Cell(int value, boolean isEmpty) {
 		this.value = value;
 		this.isEmpty = isEmpty;
 	}
 
+	// Gets the value of the cell
 	public int getValue() {
 		return value;
 	}
 
+	// Sets the value of the cell
 	public void setValue(int value) {
 		this.value = value;
 	}
 
+	// Checks if the cell is empty
 	public boolean isEmpty() {
 		return isEmpty;
 	}
-
+	
+	// Set the value of isEmpty
 	public void setEmpty(boolean isEmpty) {
 		this.isEmpty = isEmpty;
 	}
