3e97ead0c11aae712f1e0c1470f9091cf4585831
zabrigo <zoeyabrigo@gmail.com>
2018-10-25 14:41:08 -0500

    fixed solver

--- src/application/model/Solver.java
+++ src/application/model/Solver.java
@@ -27,7 +27,29 @@ public class Solver {
 		return iRowNr;
 	}
 	
-	//public int checkColsforZeros()
+	public int checkColsforZeros(Cell[][] boardArr){
+		
+		int zCount = 0;
+		int minZero = 0; 
+		int iColNr = 0;
+		
+		for(int i = 0; i < 9; i++){
+			for (int j = 0; j < 9; j++){
+				
+				if(boardArr[j][i].getValue() == 0)
+					zCount++;
+			}
+			
+			if(minZero == 0)
+				minZero = zCount;
+			else if (zCount < minZero){
+				minZero = zCount;
+				iColNr = i;
+			}
+		}
+		
+		return iColNr;
+	}
 	
 	
 
