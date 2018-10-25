6850048ed2fd6a7f0889071c382ea14074893728
Carolina Kimbrough <carolinamargaritak@gmail.com>
2018-10-25 15:56:38 -0500

    fxml stuff

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -21,6 +21,7 @@ public class MainController implements EventHandler<ActionEvent> {
 	@FXML
 	ComboBox <String> choice;
 	ArrayList<String> puzzleListAL = new ArrayList<String>();
+	final String filename = "puzzles.txt";
 	
 	@Override
 	public void handle(ActionEvent event) {
@@ -30,51 +31,27 @@ public class MainController implements EventHandler<ActionEvent> {
 	}
 	
 	/** Whenever someone makes a choice on the combobox **/
-	public void comboHandle(ActionEvent event) {
-		System.out.println("hello");
-		//Board board = Board.loadBoard(file, gridName);
-		
+	public void handleCBx(ActionEvent event) {
+		// Get the choice
+		String gridName = choice.getSelectionModel().selectedItemProperty().getValue();
+		// Get the board
+		Board board = Board.loadBoard(filename, gridName);
+
+
 	}
-	
+
 	/**  This method loads automatically when program is run **/
 	public void initialize() {
 		
-		loadPuzzles();
-		/*
-		System.out.println("\nThis method loads automatically when program is run\n");
+		loadPuzzlesCBx(); // Load the puzzles into combobox
 		
-		String file = "puzzles.txt"; // Name of the file
-		String gridName = "Grid 01"; // Should take any Grid that exists; also there's still 10 in the puzzles.txt which eh we don't need to change?
-		
-		Board board = Board.loadBoard(file, gridName); // Get that board!
-		
-		// Print the board!
-		if(board != null)
-			// PRINT 
-			System.out.println(board.getName());
-		
-			for(int a=0; a<9; a++) 
-			{
-				for(int b=0; b<9; b++) 
-				{
-					if(((b+1) % 3) == 0)
-						System.out.print(" "+ board.getArr()[a][b] + "|");
-					else
-						System.out.print(" "+ board.getArr()[a][b] + ".");
-				}
-				if(((a+1) % 3) == 0)
-					System.out.print("\n--------------------------\n");
-				else
-					System.out.println(" ");
-			}
-			// END PRINT */
 	}
 	
 	/** Loads the puzzles in the file to fill the comboBox with values **/
-	public void loadPuzzles() {
+	public void loadPuzzlesCBx() {
 		// Open puzzles.txt
 		try {
-			Scanner puzzlesFile = new Scanner(new FileReader("puzzles.txt"));  // Open
+			Scanner puzzlesFile = new Scanner(new FileReader(filename));  // Open
 			Pattern pattern = Pattern.compile("(Grid [0-9]+)$");			   // Pattern must match "Grid #"
 			Matcher match;													   // For found matches
 			// Look for matches
