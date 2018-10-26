e0bc686eb078e044c94164526fcf19247b5893bf
Carolina Kimbrough <carolinamargaritak@gmail.com>
2018-10-26 09:12:10 -0500

    finally got the numbers to show on the grid

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -36,20 +36,27 @@ public class MainController implements EventHandler<ActionEvent> {
 		  r8c0, r8c1, r8c2, r8c3, r8c4, r8c5, r8c6, r8c7, r8c8; 
 	
 	// Sudoku cells in a 2d array
-	Label [][] labels = {{r0c0, r0c1, r0c2, r0c3, r0c4, r0c5, r0c6, r0c7, r0c8},
-						 {r1c0, r1c1, r1c2, r1c3, r1c4, r1c5, r1c6, r1c7, r1c8},
-			  			 {r2c0, r2c1, r2c2, r2c3, r2c4, r2c5, r2c6, r2c7, r2c8},
-			  			 {r3c0, r3c1, r3c2, r3c3, r3c4, r3c5, r3c6, r3c7, r3c8},
-			  			 {r4c0, r4c1, r4c2, r4c3, r4c4, r4c5, r4c6, r4c7, r4c8},
-			  			 {r5c0, r5c1, r5c2, r5c3, r5c4, r5c5, r5c6, r5c7, r5c8},
-			  			 {r6c0, r6c1, r6c2, r6c3, r6c4, r6c5, r6c6, r6c7, r6c8},
-			  			 {r7c0, r7c1, r7c2, r7c3, r7c4, r7c5, r7c6, r7c7, r7c8},
-			  			 {r8c0, r8c1, r8c2, r8c3, r8c4, r8c5, r8c6, r8c7, r8c8}}; 
+	public static Label [][] labels;
+	
 
 	/**  This method loads automatically when program is run **/
 	public void initialize() {
-		
+
 		loadPuzzlesCBx(); // Load the puzzles into combobox
+
+		// Temp 2d array to get these labels into an array
+		Label [][] labelsinit = {{r0c0, r0c1, r0c2, r0c3, r0c4, r0c5, r0c6, r0c7, r0c8},
+								{r1c0, r1c1, r1c2, r1c3, r1c4, r1c5, r1c6, r1c7, r1c8},
+								{r2c0, r2c1, r2c2, r2c3, r2c4, r2c5, r2c6, r2c7, r2c8},
+								{r3c0, r3c1, r3c2, r3c3, r3c4, r3c5, r3c6, r3c7, r3c8},
+								{r4c0, r4c1, r4c2, r4c3, r4c4, r4c5, r4c6, r4c7, r4c8},
+								{r5c0, r5c1, r5c2, r5c3, r5c4, r5c5, r5c6, r5c7, r5c8},
+								{r6c0, r6c1, r6c2, r6c3, r6c4, r6c5, r6c6, r6c7, r6c8},
+								{r7c0, r7c1, r7c2, r7c3, r7c4, r7c5, r7c6, r7c7, r7c8},
+								{r8c0, r8c1, r8c2, r8c3, r8c4, r8c5, r8c6, r8c7, r8c8}}; 
+
+		// Have the public static Labels point to it; this works for whatever reason
+		labels = labelsinit; 
 		
 		
 	}
@@ -64,14 +71,13 @@ public class MainController implements EventHandler<ActionEvent> {
 	/** Method to load sudoku numbers onto gui **/
 	public void loadNums(Board board) {
 		
-		//for(int row=0; row<9; row++) {
-			//for(int col=0; col<9; col++) {
-				//System.out.println(labels[row][col].getId());
-				//labels[row][col].setText(String.valueOf(board.getArr()[row][col].getValue()));
-			//}
-		//}
-		
-		
+		// Get those numbers onto the board
+		for(int row=0; row<9; row++) {
+			for(int col=0; col<9; col++) {
+				labels[row][col].setText(String.valueOf(board.getArr()[row][col]));
+			}
+		}
+	
 	}
 	
 	/** Whenever someone makes a choice on the combobox **/
